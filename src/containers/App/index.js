babfee169100b32e6e5b32aa8cbc102e430aa36d
SeanWangx <1971822513@qq.com>
2019-02-12 18:35:44 +0800

    change config

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './index.css';
-
+import Login from '@/containers/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Login />
       </div>
     );
   }
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Input } from 'antd';
 
 class Login extends Component {
   render () {
     return (
       <div className="Login">
-        
+        <Input placeholder="Access Key"/>
+        <Input placeholder="Secret Key"/>
       </div>
     )
   }
