d27177558e6e1c403819e4e25de6d4a0f47230e2
SeanWangx <1971822513@qq.com>
2019-02-13 22:48:45 +0800

    add login form

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
-import { Input } from 'antd';
+import { Form, Input, Button } from 'antd';
 
 import './index.css';
 
 class Login extends Component {
+  constructor (props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
   render () {
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 },
+      },
+    };
+    const { getFieldDecorator } = this.props.form;
     return (
       <div className="Login">
-        <Input placeholder="Access Key"/>
-        <Input placeholder="Secret Key"/>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Item label="AccessKey" {...formItemLayout}>
+            {getFieldDecorator('accessKey', {
+              rules: [{ required: true, message: 'Please input your Access Key!' }],
+            })(
+              <Input placeholder="AccessKey" />
+            )}
+          </Form.Item>
+          <Form.Item label="SecretKey" {...formItemLayout}>
+            {getFieldDecorator('secretKey', {
+              rules: [{ required: true, message: 'Please input your Secret Key!' }],
+            })(
+              <Input.Password placeholder="SecretKey" />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">Login</Button>
+          </Form.Item>
+        </Form>
       </div>
-    )
+    );
   }
 }
 
-export default Login;
+export default Form.create({})(Login);
