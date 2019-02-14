add93885db1c7c4824f401fb396d95ce14fbe7d6
SeanWangx <1971822513@qq.com>
2019-02-14 15:09:24 +0800

    login page layout

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -14,6 +14,7 @@ class Login extends Component {
     this.props.form.validateFields((err, values) => {
       if (!err) {
         console.log('Received values of form: ', values);
+        // TODO
       }
     });
   }
@@ -32,25 +33,27 @@ class Login extends Component {
     const { getFieldDecorator } = this.props.form;
     return (
       <div className="Login">
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Item label="AccessKey" {...formItemLayout}>
-            {getFieldDecorator('accessKey', {
-              rules: [{ required: true, message: 'Please input your Access Key!' }],
-            })(
-              <Input placeholder="AccessKey" />
-            )}
-          </Form.Item>
-          <Form.Item label="SecretKey" {...formItemLayout}>
-            {getFieldDecorator('secretKey', {
-              rules: [{ required: true, message: 'Please input your Secret Key!' }],
-            })(
-              <Input.Password placeholder="SecretKey" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">Login</Button>
-          </Form.Item>
-        </Form>
+        <div className="Login-Content">
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Item label="AccessKey" {...formItemLayout}>
+              {getFieldDecorator('accessKey', {
+                rules: [{ required: true, message: 'Please input your Access Key!' }],
+              })(
+                <Input placeholder="AccessKey" />
+              )}
+            </Form.Item>
+            <Form.Item label="SecretKey" {...formItemLayout}>
+              {getFieldDecorator('secretKey', {
+                rules: [{ required: true, message: 'Please input your Secret Key!' }],
+              })(
+                <Input.Password placeholder="SecretKey" />
+              )}
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">Login</Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     );
   }
