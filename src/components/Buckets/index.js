346ae31bfb707b7f4476246b0f76481cac94f0cf
SeanWangx <1971822513@qq.com>
2019-02-20 13:25:24 +0800

    add Sider

--- src/components/Buckets/index.js
+++ src/components/Buckets/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { Layout, Icon } from 'antd';
 
 import './index.css';
+
+const { Sider, Content } = Layout;
 
 class Buckets extends Component {
   render() {
     return (
       <div className="Buckets">
-        Buckets
+        <Layout className="BucketsLayout">
+          <Sider className="BucketsSider">
+            <div className="BucketsList">
+              Buckets
+            </div>
+            <div className="BucketsSetting">
+              <Icon type="setting" className="BucketsSettingBtn"/>
+            </div>
+          </Sider>
+          <Content className="BucketsContent">Content</Content>
+        </Layout>
       </div>
     );
   }
