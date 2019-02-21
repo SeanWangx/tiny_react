0f057bc02a59d2d4b49cd3aec9ed6d2ce5c469d2
SeanWangx <1971822513@qq.com>
2019-02-21 22:39:13 +0800

    add bucket select

--- src/components/Buckets/index.js
+++ src/components/Buckets/index.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import { Layout, Icon } from 'antd';
+import { Layout, Icon, Menu } from 'antd';
 
 import './index.css';
 
 const { Sider, Content } = Layout;
 
 class Buckets extends Component {
+  constructor (props) {
+    super(props);
+    this.selectBucket = this.selectBucket.bind(this);
+  }
+  selectBucket (index) {
+    console.log(index);
+  }
   render() {
+    console.log(this.props);
+    const { buckets = [] } = this.props;
     return (
       <div className="Buckets">
         <Layout className="BucketsLayout">
           <Sider className="BucketsSider">
             <div className="BucketsList">
-              Buckets
+              <Menu theme="dark" style={{'backgroundColor': '#545c64'}}>
+                {
+                  buckets.map((item, index) => {
+                    return (
+                      <Menu.Item key={index} onClick={() => this.selectBucket(index)}>
+                        <span>{ item.name }</span>
+                      </Menu.Item>
+                    );
+                  })
+                }
+              </Menu>
             </div>
-            <div className="BucketsSetting">
-              <Icon type="setting" className="BucketsSettingBtn"/>
+            <div className="BucektsBtnRow">
+              <Icon type="plus-circle" className="BucketsBtn"/>
+            </div>
+            <div className="BucektsBtnRow" style={
+              {'position': 'absolute', 'left':'0', 'bottom': '0'}
+            }>
+              <Icon type="setting" className="BucketsBtn"/>
             </div>
           </Sider>
           <Content className="BucketsContent">Content</Content>
--- src/containers/BucketsPage.js
+++ src/containers/BucketsPage.js
@@ -4,14 +4,15 @@ import { Redirect } from 'react-router-dom';
 import Buckets from '../components/Buckets';
 
 const mapStateToProps = (state, ownProps) => ({
-  isAuth: state.isAuth
+  isAuth: state.isAuth,
+  buckets: state.buckets
 })
 
 class BucketsPage extends Component {
   render () {
-    const { isAuth } = this.props;
+    const { isAuth, buckets } = this.props;
     if (isAuth !== true) return (<Redirect to="/login" />);
-    return (<Buckets />);
+    return (<Buckets buckets={buckets}/>);
   }
 }
 
