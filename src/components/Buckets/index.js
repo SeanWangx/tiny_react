7cba3cb432c551b144c95a98672db74af3dffa6f
SeanWangx <1971822513@qq.com>
2019-02-23 20:39:35 +0800

    restructure bucket sider

--- src/components/Buckets/index.js
+++ src/components/Buckets/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Icon, Menu } from 'antd';
-
+import { Layout } from 'antd';
 import './index.css';
 
 const { Sider, Content } = Layout;
@@ -8,41 +7,20 @@ const { Sider, Content } = Layout;
 class Buckets extends Component {
   constructor (props) {
     super(props);
-    this.selectBucket = this.selectBucket.bind(this);
+    this.state = {
+      filterText: ''
+    };
   }
-  selectBucket (index) {
-    console.log(index);
-  }
-  render() {
-    console.log(this.props);
-    const { buckets = [] } = this.props;
+  render () {
     return (
-      <div className="Buckets">
-        <Layout className="BucketsLayout">
-          <Sider className="BucketsSider">
-            <div className="BucketsList">
-              <Menu theme="dark" style={{'backgroundColor': '#545c64'}}>
-                {
-                  buckets.map((item, index) => {
-                    return (
-                      <Menu.Item key={index} onClick={() => this.selectBucket(index)}>
-                        <span>{ item.name }</span>
-                      </Menu.Item>
-                    );
-                  })
-                }
-              </Menu>
-            </div>
-            <div className="BucektsBtnRow">
-              <Icon type="plus-circle" className="BucketsBtn"/>
-            </div>
-            <div className="BucektsBtnRow" style={
-              {'position': 'absolute', 'left':'0', 'bottom': '0'}
-            }>
-              <Icon type="setting" className="BucketsBtn"/>
-            </div>
+      <div className="buckets-container">
+        <Layout className="buckets-layout">
+          <Sider className="buckets-sider">
+            Sider
           </Sider>
-          <Content className="BucketsContent">Content</Content>
+          <Content className="buckets-content">
+            Content
+          </Content>
         </Layout>
       </div>
     );
