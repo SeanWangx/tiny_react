a31e211ba44312387621fe41c9e044194215c6f2
SeanWangx <1971822513@qq.com>
2019-02-23 21:13:59 +0800

    add BucketSider component

---
+++ src/components/BucketSider/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+
+const BucketSider = () => (
+  <div className="bucket-sider">
+    <div className="bucket-sider-item item-filter">
+      filter
+    </div>
+    <div className="bucket-sider-item item-list">
+      list
+    </div>
+    <div className="bucket-sider-item item-setting">
+      setting
+    </div>
+  </div>
+)
+
+export default BucketSider;
--- src/components/Buckets/index.js
+++ src/components/Buckets/index.js
@@ -1,30 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
+import BucketSider from '../BucketSider';
 import './index.css';
 
 const { Sider, Content } = Layout;
 
-class Buckets extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      filterText: ''
-    };
-  }
-  render () {
-    return (
-      <div className="buckets-container">
-        <Layout className="buckets-layout">
-          <Sider className="buckets-sider">
-            Sider
-          </Sider>
-          <Content className="buckets-content">
-            Content
-          </Content>
-        </Layout>
-      </div>
-    );
-  }
-}
+const Buckets = () => (
+  <div className="buckets-container">
+    <Layout className="buckets-layout">
+      <Sider className="buckets-sider">
+        <BucketSider />
+      </Sider>
+      <Content className="buckets-content">
+        Content
+      </Content>
+    </Layout>
+  </div>
+)
 
 export default Buckets;
