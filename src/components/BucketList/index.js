38ca7b99d48d1708a96caa99ed7ecfed541b334e
SeanWangx <1971822513@qq.com>
2019-02-24 21:50:59 +0800

    add click redirect for bucketItem

--- src/components/BucketList/index.js
+++ src/components/BucketList/index.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link, withRouter } from 'react-router-dom';
 import BucketItem from '../BucketItem';
 import './index.css';
 
 class BucketList extends Component {
   constructor (props) {
     super(props);
+    super(props);
     this.state = { activeIndex: this.props.buckets.length > 0 ? 0 : -1 };
     this.handleSelect = this.handleSelect.bind(this);
   }
-  handleSelect (e, index) {
-    e.preventDefault();
+  handleSelect (index) {
     this.setState({
       activeIndex: index
     })
   }
 
   render () {
-    const { buckets, filterText } = this.props;
+    const { buckets, filterText, match } = this.props;
     return (
       <div className="bucket-list">
         {
           buckets.map((item, index) => {
             return (
-              <BucketItem
-                show={item['name'].indexOf(filterText) !== -1}
-                active={index === this.state.activeIndex}
-                key={index} text={item['name']}
-                onSelect={(e) => this.handleSelect(e, index)}/>
+              <Link to={`${match.path}/${index}`} key={index}>
+                <BucketItem
+                  show={item['name'].indexOf(filterText) !== -1}
+                  active={index === this.state.activeIndex}
+                  text={item['name']}
+                  onSelect={() => this.handleSelect(index)}/>
+              </Link>
             );
           })
         }
@@ -52,4 +55,4 @@ BucketList.defaultProps = {
   buckets: []
 };
 
-export default BucketList;
+export default withRouter(BucketList);
