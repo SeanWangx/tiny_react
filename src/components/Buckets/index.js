0d1b4079a49879b95065c21df696586619cc94d6
SeanWangx <1971822513@qq.com>
2019-02-25 17:46:11 +0800

    add view change control state for setting

--- src/components/Buckets/index.js
+++ src/components/Buckets/index.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Layout } from 'antd';
 import BucketSider from '../BucketSider';
 import BucketContent from '../BucketContent';
+import SettingPage from '../../containers/SettingPage';
 import './index.css';
 
 const { Sider, Content } = Layout;
-
-const Buckets = () => (
-  <div className="buckets-container">
-    <Layout className="buckets-layout">
-      <Sider className="buckets-sider">
-        <BucketSider />
-      </Sider>
-      <Content className="buckets-content">
-        <BucketContent />
-      </Content>
-    </Layout>
-  </div>
-)
+class Buckets extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { view: 'content' };
+    this.viewChange = this.viewChange.bind(this);
+  }
+  viewChange (view = 'content') {
+    this.setState({view});
+  }
+  render () {
+    const { view } = this.state;
+    return (
+      <div className="buckets-container">
+        <Layout className="buckets-layout">
+          <Sider className="buckets-sider">
+            <BucketSider onViewChange={this.viewChange}/>
+          </Sider>
+          <Content className="buckets-content">
+            {
+              view === 'setting' ? (
+                <SettingPage onBack={() => this.viewChange('content')}/>
+              ) : (
+                <BucketContent />
+              )
+            }
+          </Content>
+        </Layout>
+       </div>
+     );
+  }
+}
 
 export default Buckets;
