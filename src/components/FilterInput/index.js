7a88dd8fb20283c41b32267e83e7e0e51784dc0d
SeanWangx <1971822513@qq.com>
2019-02-26 20:03:29 +0800

    remove modal from filterInput to sider

--- src/components/FilterInput/index.js
+++ src/components/FilterInput/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Input, Icon, Modal } from 'antd';
+import { Input, Icon } from 'antd';
 
 import './index.css';
 
@@ -13,50 +13,16 @@ const btnStyle = {
   'cursor': 'pointer'
 };
 
-class FilterInput extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {visible: false};
-    this.showModal = this.showModal.bind(this);
-    this.handleOk = this.handleOk.bind(this);
-    this.handleCancel= this.handleCancel.bind(this);
-  }
-  showModal () {
-    this.setState({visible: true});
-  }
-  handleOk (e) {
-    e.preventDefault();
-    this.setState({visible: false});
-    console.log('[TODO]ok');
-  }
-  handleCancel (e) {
-    e.preventDefault();
-    this.setState({visible: false});
-    console.log('[TODO]cancel');
-  }
-  addBucket () {
-    console.log('add bucket');
-  }
-  render () {
-    const { onFilter } = this.props;
-    return (
-      <div className="filter-container">
-        <div className="bucket-filter">
-          <Input size="small" placeholder="Search" style={inputStyle} onChange={onFilter}/>
-        </div>
-        <div className="bucket-add">
-          <Icon type="plus-square" style={btnStyle} onClick={this.showModal}/>
-        </div>
-        <Modal
-          title="add buckets"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}>
-        </Modal>
-      </div>
-    );
-  }
-}
+const FilterInput = ({ onFilter, onAdd }) => (
+  <div className="filter-container">
+    <div className="bucket-filter">
+      <Input size="small" placeholder="Search" style={inputStyle} onChange={onFilter}/>
+    </div>
+    <div className="bucket-add">
+      <Icon type="plus-square" style={btnStyle} onClick={onAdd} />
+    </div>
+  </div>
+)
 
 FilterInput.propTypes = {
   onFilter: PropTypes.func,
