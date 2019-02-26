5077cb681acd4c1573ac07c9b5dd5e64ddce53bf
SeanWangx <1971822513@qq.com>
2019-02-26 17:39:46 +0800

    add modal from create bucket

--- src/components/FilterInput/index.js
+++ src/components/FilterInput/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Input, Icon } from 'antd';
+import { Input, Icon, Modal } from 'antd';
 
 import './index.css';
 
@@ -13,16 +13,50 @@ const btnStyle = {
   'cursor': 'pointer'
 };
 
-const FilterInput = ({onFilter, onAdd}) => (
-  <div className="filter-container">
-    <div className="bucket-filter">
-      <Input size="small" placeholder="Search" style={inputStyle} onChange={onFilter}/>
-    </div>
-    <div className="bucket-add">
-      <Icon type="plus-square" style={btnStyle} onClick={onAdd}/>
-    </div>
-  </div>
-)
+class FilterInput extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {visible: false};
+    this.showModal = this.showModal.bind(this);
+    this.handleOk = this.handleOk.bind(this);
+    this.handleCancel= this.handleCancel.bind(this);
+  }
+  showModal () {
+    this.setState({visible: true});
+  }
+  handleOk (e) {
+    e.preventDefault();
+    this.setState({visible: false});
+    console.log('[TODO]ok');
+  }
+  handleCancel (e) {
+    e.preventDefault();
+    this.setState({visible: false});
+    console.log('[TODO]cancel');
+  }
+  addBucket () {
+    console.log('add bucket');
+  }
+  render () {
+    const { onFilter } = this.props;
+    return (
+      <div className="filter-container">
+        <div className="bucket-filter">
+          <Input size="small" placeholder="Search" style={inputStyle} onChange={onFilter}/>
+        </div>
+        <div className="bucket-add">
+          <Icon type="plus-square" style={btnStyle} onClick={this.showModal}/>
+        </div>
+        <Modal
+          title="add buckets"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 FilterInput.propTypes = {
   onFilter: PropTypes.func,
@@ -35,3 +69,4 @@ FilterInput.defaultProps = {
 };
 
 export default FilterInput;
+
