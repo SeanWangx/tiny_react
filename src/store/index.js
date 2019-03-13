c1d6c0dd798d97c41cbcc8c0706e102cf18b0adb
SeanWangx <1971822513@qq.com>
2019-03-13 16:54:12 +0800

    add redux thunl

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import tinyApp from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   tinyApp,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(
+      thunk
+    )
+  )
 );
 
 export default store;
