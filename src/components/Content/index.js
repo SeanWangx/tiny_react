f812af425bbeade08535796e9e494778cac445d4
SeanWangx <1971822513@qq.com>
2019-03-30 11:19:15 +0800

    add base component for Contnet: HasBucket Upload

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import NoBucket from '../../containers/NoBucket';
 
 import './index.css';
 
-const Content = ({ bucketSelected }) => (
-  <div className="bucket-content">
-    {
-      bucketSelected === '' ? <NoBucket /> : bucketSelected
-    }
+const Upload = ({ onBack }) => (
+  <div>
+    <h1>Upload</h1>
+    <p><input type="button" value="Back" onClick={onBack}/></p>
   </div>
 );
+const HasBucket = ({ bucket, onUpload }) => (
+  <div>
+    <h1>{ bucket }</h1>
+    <p><input type="button" value="Upload" onClick={onUpload}/></p>
+  </div>
+);
+
+class Content extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      view: 'source', // or 'upload'
+    };
+  }
+  back = (e) => {
+    e.preventDefault();
+    this.setState({ view: 'source' });
+  }
+  upload = (e) => {
+    e.preventDefault();
+    this.setState({ view: 'upload' });
+  }
+  render () {
+    return (
+      <div className="bucket-content">
+        {
+          this.props.bucketSelected === ''
+            ? <NoBucket />
+            : (
+              this.state.view === 'upload'
+                ? <Upload onBack={this.back}/>
+                : <HasBucket bucket={this.props.bucketSelected} onUpload={this.upload}/>
+            )
+        }
+      </div>
+    );
+  }
+}
 
 Content.propTypes = {
   bucketSelected: PropTypes.string
