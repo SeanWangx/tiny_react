5e57532de3eb7944db2c910a92321c2661d1dff2
SeanWangx <1971822513@qq.com>
2019-03-31 15:05:45 +0800

    remove fetchBucketList in Manager

--- src/containers/Manager.js
+++ src/containers/Manager.js
@@ -1,16 +1,11 @@
 import { connect } from 'react-redux';
 import Manager from '../components/Manager';
-import { fetchBucketList } from '../store/actions';
 
 const mapStateToProps = (state, ownProps) => ({
   isAuth: state.baseConfig.isAuth
 })
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  fetchBucketList: () => {
-    return dispatch(fetchBucketList());
-  }
-})
+const mapDispatchToProps = (dispatch, ownProps) => ({})
 
 export default connect(
   mapStateToProps,
